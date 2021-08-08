d2e0e24e8a11f23116158e55cf728f9d58b46403
Ranjit67 <ranjitsahoo873@gmail.com>
2021-08-08 11:39:11 +0530

    stun apdate and restart update

--- app.js
+++ app.js
@@ -365,6 +365,33 @@ const createPeer = (socket) => {
       {
         urls: "stun:stun.stunprotocol.org",
       },
+      { urls: "stun:stun.ekiga.net" },
+      { urls: "stun:stun.schlund.de" },
+      { urls: "stun:stun.l.google.com:19302" },
+
+      { urls: "stun:stun1.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:19302" },
+      { urls: "stun:stun.softjoys.com" },
+      { urls: "stun:stun.voipbuster.com" },
+      { urls: "stun:stun.voipstunt.com" },
+      { urls: "stun:stun.xten.com" },
+      {
+        urls: "turn:numb.viagenie.ca",
+        credential: "muazkh",
+        username: "webrtc@live.com",
+      },
+      {
+        urls: "turn:192.158.29.39:3478?transport=udp",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        username: "28224511:1379330808",
+      },
+      {
+        urls: "turn:192.158.29.39:3478?transport=tcp",
+        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        username: "28224511:1379330808",
+      },
     ],
   });
   peer.ontrack = (e) => handleTrackEvent(e, peer);
@@ -392,7 +419,11 @@ const createPeer = (socket) => {
       onconnectionstatechange: event,
     });
   };
-  peer.oniceconnectionstatechange = (event) => {
+  peer.oniceconnectionstatechange = async (event) => {
+    if (peer?.iceConnectionState === "failed") {
+      await peer.restartIce();
+      console.log("hit restart");
+    }
     // console.log("oniceconnectionstatechange ", event);
     // console.log(
     //   "pc.current.iceConnectionState ",
